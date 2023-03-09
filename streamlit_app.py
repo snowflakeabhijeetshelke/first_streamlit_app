4cf78eb0dab1c0309cefe1f2ea3b9b3f86688c0f
snowflakeabhijeetshelke <127420534+snowflakeabhijeetshelke@users.noreply.github.com>
2023-03-09 16:01:06 +0000

    Update streamlit_app.py
    Added more menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Brekfast Menu')
-streamlit.title('Omega 3 & Blueberry oatmeal')
+streamlit.header('Brekfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach 3 & Rocket Smoothie')
+streamlit.text('Hard-Bolied Free-Range Egg')
