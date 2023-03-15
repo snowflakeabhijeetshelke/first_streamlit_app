1990763c58a2839703ba455e1306f55113b83d57
snowflakeabhijeetshelke <127420534+snowflakeabhijeetshelke@users.noreply.github.com>
2023-03-15 20:15:40 +0000

    Update streamlit_app.py
    added trial account metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 import pandas
-import snowflake.connector
+
 streamlit.title('My Mom'"'"'s New Healthy Diner')
 
 streamlit.header('Brekfast Favorites')
@@ -37,3 +37,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as table 
 streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
